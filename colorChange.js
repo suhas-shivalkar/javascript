60a113a1c9be4f1fd4b206e1346c5944767f4bee
suhas shivalkar <pavanshivalkar@gmail.com>
2024-07-10 15:41:41 +0530

    using switch

--- colorChange.js
+++ colorChange.js
@@ -5,17 +5,33 @@ const body=document.querySelector("body")
 buttons.forEach(function (button){
     button.addEventListener('click',(e)=>{
         // console.log(e.target);
-        if (e.target.id=='grey') {
-            body.style.backgroundColor=e.target.id
+        switch (e.target.id) {
+            case 'grey':
+                body.style.backgroundColor=e.target.id
+                break;
+            case 'blue':
+                    body.style.backgroundColor=e.target.id
+                    break;
+            case 'yellow':
+                        body.style.backgroundColor=e.target.id
+                        break;
+            case 'white':
+                            body.style.backgroundColor=e.target.id
+                            break;        
+            default:
+                break;
         }
-        if (e.target.id=='blue') {
-            body.style.backgroundColor=e.target.id
-        }
-        if (e.target.id=='yellow') {
-            body.style.backgroundColor=e.target.id
-        }
-        if (e.target.id=='white') {
-            body.style.backgroundColor=e.target.id;
-        }
+        // if (e.target.id=='grey') {
+        //     body.style.backgroundColor=e.target.id
+        // }
+        // if (e.target.id=='blue') {
+        //     body.style.backgroundColor=e.target.id
+        // }
+        // if (e.target.id=='yellow') {
+        //     body.style.backgroundColor=e.target.id
+        // }
+        // if (e.target.id=='white') {
+        //     body.style.backgroundColor=e.target.id;
+        // }
     })
 })
